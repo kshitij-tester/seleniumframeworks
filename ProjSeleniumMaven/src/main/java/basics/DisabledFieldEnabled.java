8de969ae1ca384c144827f5ff02a15921c947d69
kshitijsalgarqa <kshitijsalgarqa@localhost>
2025-05-16 20:38:51 +0530

    Updated existing code

--- ProjSeleniumMaven/src/main/java/basics/DisabledFieldEnabled.java
+++ ProjSeleniumMaven/src/main/java/basics/DisabledFieldEnabled.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class DisabledFieldEnabled {
@@ -12,7 +13,7 @@ public class DisabledFieldEnabled {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
-		WebDriver d=new ChromeDriver();
+		WebDriver d=new FirefoxDriver();
 		d.get("https://proposalfinder.com/create-wedding-profile");
 		Thread.sleep(2000);
 		
